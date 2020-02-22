f889b281e14a1ce33834559b3f544ee6d17eff2c
Aman Kapur <akapur07@gmail.com>
2020-02-22 13:43:51 +0530

    Level - 3

--- spring-boot-config/spring-boot-config/src/main/java/io/learn/springbootconfig/resource/MyController.java
+++ spring-boot-config/spring-boot-config/src/main/java/io/learn/springbootconfig/resource/MyController.java
@@ -2,6 +2,7 @@ package io.learn.springbootconfig.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RefreshScope
 public class MyController {
     @Value("${my.app.name}")
     private String appName;
